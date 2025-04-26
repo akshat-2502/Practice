bb2c721136792b29d5f14b2c0a0dbde1b73eeb5a
akshat-2502 <khulbeakshat@gmail.com>
2025-04-27 02:49:10 +0530

    completed

---
+++ src/COMPONENTS/Tasks.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+function Tasks({ task, toggleComplete }) {
+  return (
+    <>
+      {task.map((taskitems) => (
+        <div
+          key={taskitems.id}
+          className="eachTask w-full h-[7vh] mt-5 mb-5 bg-zinc-600 rounded-lg px-4 py-3 flex justify-between items-center"
+        >
+          {/* ✅ Strike-through if completed */}
+          <p
+            className={`text-lg ${
+              taskitems.completed ? "line-through text-gray-400" : ""
+            }`}
+          >
+            {taskitems.name}
+          </p>
+
+          <div className="taskAction flex items-center justify-between gap-5">
+            <input
+              type="checkbox"
+              checked={taskitems.completed}
+              onChange={() => toggleComplete(taskitems.id)}
+              className="w-6 h-6 accent-green-500 border-2"
+            />
+            <button className="px-10 text-white hover:bg-red-600 py-2 bg-red-400 rounded-2xl">
+              DELETE
+            </button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default Tasks;
