422a0c9fecb0315907f7a694fc4e1547b0b19768
akshat-2502 <khulbeakshat@gmail.com>
2025-06-13 21:40:54 +0530

    folders names are shown using the recursion

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import json from "./data.json";
+
+const List = ({ list }) => {
+  return (
+    <div className="container">
+      {list.map((node) => (
+        <div className="pl-8">
+          <span key={node.id}>{node.name}</span>
+          {node?.children && <List list={node.children} />}
+        </div>
+      ))}
+    </div>
+  );
+};
+
 const App = () => {
+  const [data, setData] = useState(json);
+  console.log(data);
+
   return (
     <div>
       <h1>File Explorer</h1>
+      <List list={data} />
     </div>
   );
 };
